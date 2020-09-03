b01fda21ef80e5b6da4298e469dd9c74910a9415
tckelleyspace <32001775+tckelleyspace@users.noreply.github.com>
2020-09-03 15:39:03 -0500

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! My link is working now. Testing 123
+          Hi there! My link is working now. I was changed on the feature branch.
         </p>
         <a
           className="App-link"
